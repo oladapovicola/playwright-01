78b7f230a280715bccff63737f12495298fab66c
oladapo.aiyenitaju <dapssyola@gmail.com>
2025-08-12 13:33:09 +0300

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test.only('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // check if gitHub icon is visible
+    await expect(page.getByRole('link', { name: 'GitHub repository' })).toBeVisible();
+
+    // check footer is visible
+
+  await expect(page.getByRole('contentinfo')).toBeVisible();
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
