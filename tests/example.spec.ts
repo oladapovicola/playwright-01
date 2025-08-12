721490d47f91e593005ecedc40a9905e6b97b605
oladapo.aiyenitaju <dapssyola@gmail.com>
2025-08-12 13:57:46 +0300

    HW15

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,11 +11,13 @@ test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // check if gitHub icon is visible
-    await expect(page.getByRole('link', { name: 'GitHub repository' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'GitHub repository'})).toBeVisible();
 
-    // check footer is visible
 
-  await expect(page.getByRole('contentinfo')).toBeVisible();
+
+  // check footer is visible
+
+  //await expect(page.getByRole('contentinfo')).toBeVisible();
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
